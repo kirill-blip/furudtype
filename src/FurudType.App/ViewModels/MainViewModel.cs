bc52c362d965c334416f84a652cf47f3c1af967a
Kirill Golubenko <golubenkokirill@outlook.com>
2026-05-04 20:54:16 +0500

    Implement basic typing engine with MainView and ViewModel

---
+++ src/FurudType.App/ViewModels/MainViewModel.cs
@@ -0,0 +1,18 @@
+﻿using Avalonia.Input;
+
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace FurudType.App.ViewModels;
+
+public partial class MainViewModel : ObservableObject
+{
+    [ObservableProperty]
+    private string? _textToDisplay;
+
+    [RelayCommand]
+    private void HandleTextInputCommand(TextInputEventArgs e)
+    {
+        TextToDisplay += e.Text;
+    }
+}
