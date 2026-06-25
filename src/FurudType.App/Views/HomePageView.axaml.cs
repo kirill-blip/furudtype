77d59f9f02811fb3661c4bd4ed315ffbbf97a184
Kirill Golubenko <golubenkokirill@outlook.com>
2026-06-25 23:00:52 +0500

    Add home page view and view model

---
+++ src/FurudType.App/Views/HomePageView.axaml.cs
@@ -0,0 +1,11 @@
+﻿using Avalonia.Controls;
+
+namespace FurudType.App;
+
+public partial class HomePageView : UserControl
+{
+    public HomePageView()
+    {
+        InitializeComponent();
+    }
+}
